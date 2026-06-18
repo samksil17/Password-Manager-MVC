dcc229c240a274fd30935c649399af7be7b58677
Samir Kalil <88513606+samykali17@users.noreply.github.com>
2026-06-17 23:55:28 -0500

    temp: endpoint reset para limpiar datos

--- backend/app.py
+++ backend/app.py
@@ -143,6 +143,13 @@ def get_logs():
     return jsonify(historial), 200
  
  
+ # Endpoint temporal para limpiar datos de prueba
+@app.route('/reset', methods=['GET'])
+def reset():
+    db.session.query(Credential).delete()
+    db.session.commit()
+    return jsonify({'message': 'Base de datos limpiada'}), 200
+
 # INICIO 
 if __name__ == '__main__':
     app.run(debug=True)
